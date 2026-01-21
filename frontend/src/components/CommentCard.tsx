1e7690ffe73243d8f72cc82fa33b8ce3cb17fef5
Adzfar <md.nur.adzfar@gmail.com>
2026-01-21 21:40:04 +0800

    fix: Comment formatting issue fixeed

--- frontend/src/components/CommentCard.tsx
+++ frontend/src/components/CommentCard.tsx
@@ -118,6 +118,7 @@ export default function CommentCard({
                             </Typography>
                         </Box>
 
+                        {/* Edit/Delete Buttons */}
                         {isAuthor && !isEditing && (
                             <Box
                                 sx={{
@@ -145,9 +146,6 @@ export default function CommentCard({
                             </Box>
                         )}
                     </Box>
-
-                    {/* Edit and Delete Buttons */}
-
 
                     {/* Comment Content or Edit Form */}
                     {
